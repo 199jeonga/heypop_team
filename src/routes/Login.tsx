79e96e6ba2f5fbae3a47db056549de601f0b0401
199jeonga <lja3248@gmail.com>
2022-06-15 01:23:18 +0900

    [Feat] add login and Register to newletter

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import HeaderBox from 'pages/common/header/HeaderBox'
 import FooterBox from 'pages/common/footer/FooterBox'
 import LoginBox from 'pages/login/LoginBox'
+import NewsletterBox from 'pages/home/newsletter/NewsletterBox'
 
 function Login() {
     return (
         <div>
             <HeaderBox />
             <LoginBox />
+            <NewsletterBox />
             <FooterBox />
         </div>
     )
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeaderBox from 'pages/common/header/HeaderBox'
 import FooterBox from 'pages/common/footer/FooterBox'
+import NewsletterBox from 'pages/home/newsletter/NewsletterBox'
 import RegisterBox from '../pages/register/RegisterBox'
 
 function Register() {
@@ -8,6 +9,7 @@ function Register() {
         <div>
             <HeaderBox />
             <RegisterBox />
+            <NewsletterBox />
             <FooterBox />
         </div>
     )
